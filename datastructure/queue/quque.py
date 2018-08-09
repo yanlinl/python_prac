297bf23ef4cb773c985f3c67acc56ec5674e3c33
Yanlin Li <yanlinli0016@gmail.com>
2018-08-08 17:08:26 -0700

    Implemented queue class

---
+++ datastructure/queue/quque.py
@@ -0,0 +1,29 @@
+
+# queue is a datastructure that stores data in a manner
+# that first in first out(FIFO)
+class queue:
+    def __init__(self, firstItem=None):
+        self.data = [firstItem]
+
+    def enqueue(self, item):
+        self.data.append(item)
+
+    def dequeue(self):
+        # check if queue is empty
+        if self.data[0] == None:
+            return -1;
+        else:
+            return self.data.pop(0)
+
+    def peak(self):
+        return self.data[0]
+
+# testing code
+first = queue(1)
+
+first.enqueue(2)
+print first.peak()
+
+print first.data
+
+
